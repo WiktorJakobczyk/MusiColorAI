069ecaf06f13327c5afa3dae991b622750577174
W.Jakobczyk <wiktor.jakobczyk99@gmail.com>
2021-01-15 01:14:54 +0100

    Tempo and key

--- DetectColors/EditMid.py
+++ DetectColors/EditMid.py
@@ -18,14 +18,20 @@ class EditMid:
         if not os.path.exists(output_flac_folder):
             os.makedirs(output_flac_folder)
 
-    def change_tempo(self, fctr, weight):  # fctr is percentage of input_name tempo, e.g. 120*0.5 = 60, time from 6 sec stretches to 12 secs
-        score = music21.converter.Converter()
-        score.parseFile(self.input_name)
+    def change_tempo(self, fctr, weight, key):  # fctr is percentage of input_name tempo, e.g. 120*0.5 = 60, time from 6 sec stretches to 12 secs
+        #score = music21.converter().Converter()
+        score = music21.converter.parse(self.input_name)
+        #score.parseFile(self.input_name)
         weight = 1.5 - weight
-        newscore = score.stream.scaleDurations(
-            1 / fctr).scaleOffsets(weight)  # have to inverse fctr, otherwise it will increase tempo instead of decrease and vice versa
+        newscore = score.scaleDurations(1/fctr).scaleOffsets(1/fctr)             # have to inverse fctr, otherwise it will increase tempo instead of decrease and vice versa
 
-        newscore.write('midi', self.output_midi_folder + self.output_midi_name)
+
+
+        k = newscore.analyze('key')
+        i = music21.interval.Interval(k.tonic, music21.pitch.Pitch(key))
+        newscore = newscore.transpose(i)
+
+        newscore.write('midi', self.output_midi_folder+key+'_'+self.output_midi_name)
 
     def change_scale(self, weight):
         score = music21.converter.Converter()
@@ -36,7 +42,10 @@ class EditMid:
 
     def export_to_flac(self, soundfont_path):  # works only for FLAC files
         fs = FluidSynth(soundfont_path)
-        fs.midi_to_audio(self.input_name, self.output_flac_folder + self.output_flac_name)
+        name=self.input_name[:-4]
+        # //nameFlac=name
+        print(f'TO: {name}')
+        fs.midi_to_audio(self.input_name, self.output_flac_folder +name+'.flac')
 
 
 
@@ -49,25 +58,3 @@ pyfluidsynth: https://pypi.org/project/pyFluidSynth/ or pip install pyfluidsynth
 midi2audio: https://pypi.org/project/midi2audio/ or pip install midi2audio
 """
 
-# example usage
-# --------------------------------------------------------------------------------
-# create object midi : input file,
-# midi output folder, midi filename,
-# flac output folder, flac filename
-midi = EditMid("input_midi_example_-_Aguado_12valses_Op1_No1.mid",
-                './edited_midi/', 'output.mid',
-                './edited_flac/', 'output.flac')
-
-# change tempo
-tempo = 0.5
-# midi.change_tempo(tempo)
-
-# export to flac
-midi.export_to_flac('./soundfonts/full_grand_piano.sf2')
-
-# play midi
-# Default soundfont should be in C:\Users\[username]\.fluidsynth\default_sound_font.sf2.
-# If it isn't there, just create folder and add any .sf2 file with name 'default_sound_font'
-fs = FluidSynth()
-fs.play_midi("input_midi_example_-_Aguado_12valses_Op1_No1.mid")
-# --------------------------------------------------------------------------------
--- DetectColors/main.py
+++ DetectColors/main.py
@@ -56,13 +56,16 @@ def deleteOldFiles():
     for count, filename in enumerate(os.listdir(PATH_MELODY)):
         dst = PATH_MELODY+"melody" + str(count) + ".mid"
         os.remove(dst)
+    for count, filename in enumerate(os.listdir(PATH_CHORDS)):
+        dst = PATH_CHORDS+"chord" + str(count) + ".mid"
+        os.remove(dst)
 
 
-def renameFiles():
-    for count, filename in enumerate(os.listdir(PATH_MELODY)):
-        dst = "melody" + str(count) + ".mid"
-        src = PATH_MELODY + filename
-        dst = PATH_MELODY + dst
+def renameFiles(path,name):
+    for count, filename in enumerate(os.listdir(path)):
+        dst = name + str(count) + ".mid"
+        src = path + filename
+        dst = path + dst
         os.rename(src, dst)
 
 
@@ -75,9 +78,11 @@ def addChords():
             Chords('attention_improv').addChordsHappy('melody' + str(i) + '.mid')
 if __name__ == '__main__':
 
+
+
     #  Open img, and get dominant colours from it
     #  Second parameter determines number of colors (default=8)
-    colours = getColorsFromImage("images/image4.jpg")
+    colours = getColorsFromImage("images/image2.jpg")
 
     #  Create a CSV file
     #  It will be used later for drawing a pie plot in ColorChar class.
@@ -106,37 +111,83 @@ if __name__ == '__main__':
     # Delete old midis
     deleteOldFiles()
 
+    # HAPPY:
+    # 1-0.9 C4
+    # 0.9-0.8 D4
+    # 0.8-0.7 F4
+    # 0.7-0.6 A4
+    # 0.6-0.5 C5
+    # 0.5-0.4 D5
+    # 0.4-0.3 E5
+    # 0.3-0.2 C6
+    # 0.2-0.1 D6
+    # 0.1-0 to E6
+    happyList = ['E6', 'D6', 'C6', 'E5', 'D5', 'C5', 'A4', 'F4', 'D4', 'C4']
+    # SAD:
+    # 1-0.9 C2
+    # 0.9-0.8 D2
+    # 0.8-0.7 F2
+    # 0.7-0.6 G2
+    # 0.6-0.5 A2
+    # 0.5-0.4 C3
+    # 0.4-0.3 E3
+    # 0.3-0.2 F3
+    # 0.2-0.1 B3
+    # 0.1-0 to C4
+    sadList = ['C4-', 'B3-', 'F3-', 'E3-', 'C3-', 'A2-', 'G2-', 'F2-', 'D2-', 'C2-']
+
+
     # Decide what model should be used based on averageHeat value.
     if averageHeat<=0.5:
         print(f'SAD Colors')
-        Generate('./models/modelSadLookback_rnn.mag','lookback_rnn').generate("[55]")
-        # Tempo 90-180 BPM
-        tempo = (((averageActivity - 0) * (1.5 - 0.75)) / (1 - 0)) + 0.75
-    else:
-        print(f'HAPPY Colors')
-        Generate('./models/modelHappyLookback_rnn.mag', 'lookback_rnn').generate("[65]")
+        Generate('./models/modelSadLookback_rnn.mag','lookback_rnn').generate("[60]")
+
         # Tempo 78-144 BPM
         tempo = (((averageActivity - 0) * (1.2 - 0.65)) / (1 - 0)) + 0.65
+        index=int(averageWeight*10)
+        key =sadList[index]
+
+    else:
+        print(f'HAPPY Colors')
+        Generate('./models/modelHappyLookback_rnn.mag', 'lookback_rnn').generate("[60]")
+        # Tempo 90-180 BPM
+        tempo = (((averageActivity - 0) * (1.5 - 0.75)) / (1 - 0)) + 0.75
+        index = int(averageWeight * 10)
+        print(f'index: {index}')
+        key = happyList[index]
+        print(f'key: {key}')
+
     # TODO: folders
     # Change files names so they will be easier to operate on. e.g 2021-01-06_201919_01.mid to melody0.mid
-    renameFiles()
+    renameFiles(PATH_MELODY, 'melody')
 
     # Add chords to generated melodies.
     addChords()
+
+    renameFiles(PATH_CHORDS, 'chord')
 
     # Create mp3/wav with new tempo/low-pass filter
     # TODO: temp and filter
     from DetectColors.EditMid import EditMid
 
 
-    print(f'tempo {120/tempo}')   #DEBUG ONLY
-    print(f'{120/2}--{120/0.8}')
+    print(f'tempo {tempo} "  " {120* tempo}')  # DEBUG ONLY
 
-    print(f'tempo {120 / tempo}')  # DEBUG ONLY
+
+
 
     # retrieving all files in directory PATH_CHORDS
     import glob
     os.chdir(PATH_CHORDS)
     for file in glob.glob("*.mid"):
-        midi = EditMid(file, PATH_MUSIC, file)# po przecinku dopisz folder dla plików flac i nazwy tych plików
-        midi.change_tempo(tempo,averageWeight)
+        print(f'file: {file}')
+        midi = EditMid(file, PATH_MUSIC, file,PATH_MUSIC) # po przecinku dopisz folder dla plików flac i nazwy tych plików
+        midi.change_tempo(fctr=tempo,weight=averageWeight,key=key)
+
+    os.chdir(PATH_MUSIC)
+    for file in glob.glob("*.mid"):
+        midi = EditMid(file, PATH_MUSIC, file, PATH_MUSIC)
+        midi.export_to_flac('F:/Python/NEW/MusiColorAI/DetectColors/soundfonts/full_grand_piano.sf2')
+
+    # Delete old midis
+    deleteOldFiles()
